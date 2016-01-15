4dfb27d12e81df7a57a850269db405b176ffe80a
William Yang <williamyang93@gmail.com>
2016-01-14 16:27:56 -0800

    Updated from Master, starting to figure stuff out

--- BUNBUNN/Assets/Scripts/Customize_Switch.cs
+++ BUNBUNN/Assets/Scripts/Customize_Switch.cs
@@ -1,16 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Customize_Switch : MonoBehaviour {
 
-	public Button RightArrow;
-	//public Button LeftArrow;
+    public Button RightArrow;
+    //public Button LeftArrow;
 
-	// Use this for initialization
-	void Start () {
-		RightArrow = RightArrow.GetComponent<Button>();
-		//LeftArrow = LeftArrow.GetComponent<Button>();
-	}
+    // Use this for initialization
+    void Start () {
+        //RightArrow = RightArrow.GetComponent<Button>();
+        //LeftArrow = LeftArrow.GetComponent<Button>();
+    }
 	
 	public void SinglePress()
 	{
