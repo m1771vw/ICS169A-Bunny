4d5dce0f52fef68778361e82add45922758149ea
William Yang <williamyang93@gmail.com>
2016-01-22 15:35:29 -0800

    Trying to put in images
    just disappears

--- BUNBUNN/Assets/Scripts/Customize_Switch.cs
+++ BUNBUNN/Assets/Scripts/Customize_Switch.cs
@@ -8,8 +8,11 @@ public class Customize_Switch : MonoBehaviour {
     public Button RightArrow;
     
     public Button LeftArrow;
-    
+    public Image Character1;
+    public Image Character2;
+    public Image Character3;
 
+    
     // Use this for initialization
     void Start () {
         //RightArrow = RightArrow.GetComponent<Button>();
@@ -24,6 +27,8 @@ public class Customize_Switch : MonoBehaviour {
         // Change image
         //Application.LoadLevel("Calibration");
         Debug.Log("Hello!");
-
-	}
+        Character1.enabled = false;
+        Debug.Log(Character2.enabled);
+        Character2.enabled = true;
+    }
 }
