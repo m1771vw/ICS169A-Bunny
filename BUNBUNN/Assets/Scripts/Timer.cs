3fbece8023138bd802b6aa1514a94d13864644b5
Victor Hsiao <vwhsiao@uci.edu>
2016-02-18 23:43:26 -0800

    Working on UI - Victor
    Timer is now a circle with the seconds left next to it

--- BUNBUNN/Assets/Scripts/Timer.cs
+++ BUNBUNN/Assets/Scripts/Timer.cs
@@ -11,12 +11,12 @@ public class Timer : MonoBehaviour
     private RectTransform canvas;
     public float maxTime;
     private float curTime;
-    
+    private Image timerPie;
 
     public void Start()
     {
 
-
+        timerPie = GameObject.Find("TimerPie").GetComponent<Image>();
         timeTF = gameObject.GetComponent<Text>();
         curTime = (float.Parse(timeTF.text));
         maxTime = (float.Parse(timeTF.text));
@@ -24,17 +24,17 @@ public class Timer : MonoBehaviour
         //InvokeRepeating("ReduceTime", 1, 1);
 
 
-        canvas = GameObject.Find("Canvas").GetComponent<RectTransform>();
+        //canvas = GameObject.Find("Canvas").GetComponent<RectTransform>();
 
-        GameObject timeFront = new GameObject("timeBar");
-        timeBar = timeFront.AddComponent<Image>();
-        timeBar.rectTransform.SetParent(canvas.transform, false);
-        timeBar.color = Color.green;
+        //GameObject timeFront = new GameObject("timeBar");
+        //timeBar = timeFront.AddComponent<Image>();
+        //timeBar.rectTransform.SetParent(canvas.transform, false);
+        //timeBar.color = Color.green;
 
-        GameObject timeBack = new GameObject("timeBarBack");
-        backBar = timeBack.AddComponent<Image>();
-        backBar.rectTransform.SetParent(canvas.transform, false);
-        backBar.color = new Color(0.0f, 0.0f, 0.0f);
+        //GameObject timeBack = new GameObject("timeBarBack");
+        //backBar = timeBack.AddComponent<Image>();
+        //backBar.rectTransform.SetParent(canvas.transform, false);
+        //backBar.color = new Color(0.0f, 0.0f, 0.0f);
     }
 
     public void setMaxTime(float number)
@@ -55,34 +55,41 @@ public class Timer : MonoBehaviour
             curTime = 0.0f;
         }
         float percent = curTime / maxTime;
-
+        timerPie.fillAmount = percent;
         if ((percent > 0.3) && (percent < 0.6))
         {
-            timeBar.color = Color.yellow;
+            //timeBar.color = Color.yellow;
+            timerPie.color = Color.yellow;
         }
         else if (percent < 0.3)
         {
-            timeBar.color = Color.red;
+            
+           // timeBar.color = Color.red;
+            timerPie.color = Color.red;
         }
 
-        float target, xbar = 0.0f;
-        target = Screen.width * percent;
-        xbar = Mathf.Lerp(xbar, percent, 1.75f);
-        xbar = Mathf.Lerp(xbar, percent, 2.0f);
+        //float target, xbar = 0.0f;
+        //target = Screen.width * percent;
+        //xbar = Mathf.Lerp(xbar, percent, 1.75f);
+        //xbar = Mathf.Lerp(xbar, percent, 2.0f);
 
-        timeBar.rectTransform.offsetMin = Vector2.zero;
-        timeBar.rectTransform.offsetMax = Vector2.zero;
-        timeBar.rectTransform.anchorMin = new Vector2(0.0f, 0.84f);
-        timeBar.rectTransform.anchorMax = new Vector2(xbar, 0.85f);
+        //timeBar.rectTransform.offsetMin = Vector2.zero;
+        //timeBar.rectTransform.offsetMax = Vector2.zero;
+        //timeBar.rectTransform.anchorMin = new Vector2(0.0f, 0.85f);
+        //timeBar.rectTransform.anchorMax = new Vector2(xbar, 0.86f);
 
-        backBar.rectTransform.offsetMin = Vector2.zero;
-        backBar.rectTransform.offsetMax = Vector2.zero;
-        backBar.rectTransform.anchorMin = new Vector2(xbar, 0.84f);
-        backBar.rectTransform.anchorMax = new Vector2(1.0f, 0.85f);
+        //backBar.rectTransform.offsetMin = Vector2.zero;
+        //backBar.rectTransform.offsetMax = Vector2.zero;
+        //backBar.rectTransform.anchorMin = new Vector2(xbar, 0.85f);
+        //backBar.rectTransform.anchorMax = new Vector2(1.0f, 0.86f);
 
         if (curTime<(maxTime/2))
         {
-            timeTF.text = curTime.ToString();
+
+            float newTime = Mathf.Floor(curTime * 100.0f + 0.5f) / 100;
+            
+            Debug.Log(newTime);
+            timeTF.text = newTime.ToString("F2");
         }
         else
         {
