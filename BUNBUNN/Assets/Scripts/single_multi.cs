0cb4188981e861134b9ad5c7914a2cd4627b6ec2
William Yang <williamyang93@gmail.com>
2016-02-24 17:20:47 -0800

    Trying to remove oscar's stuff

--- BUNBUNN/Assets/Scripts/single_multi.cs
+++ BUNBUNN/Assets/Scripts/single_multi.cs
@@ -1,35 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using GooglePlayGames;
 using System.Collections;
 using UnityEngine.SceneManagement;
 public class single_multi : MonoBehaviour
 {
     public Canvas quitMenu;
-    public string LoggingInAcheivment = "CgkIitHChdsBEAIQAQ";
     public Button PlaySingle;
     public Button exitText;
     public Button PlayMulti;
     public GameObject SinglePlayer;
     public GameObject Multi;
     public GameObject Quit;
-    PlayGames Gplay = new PlayGames();
+
 
     void Start()
     {
-        PlayGamesPlatform.Activate();
-        Gplay.LogIn();
-        //Gplay.AddAcheivements(LoggingInAcheivment);
         quitMenu = quitMenu.GetComponent<Canvas>();
         exitText = exitText.GetComponent<Button>();
         PlaySingle = PlaySingle.GetComponent<Button>();
         //PlayMulti = PlayMulti.GetComponent<Button>();
         quitMenu.enabled = false;
 
-    }
-    void Update()
-    {
-        Gplay.AddAcheivements(LoggingInAcheivment);
     }
     public void exitPress()
     {
