eff92a943afcc89a97c7b42b44580874b642c441
Oscar <ochng6654@gmail.com>
2016-02-29 18:16:18 -0800

    fix merge/pull issues
    fix merge/pull issues

--- BUNBUNN/Assets/Scripts/LocalMultiplayerGameData.cs
+++ BUNBUNN/Assets/Scripts/LocalMultiplayerGameData.cs
@@ -38,16 +38,11 @@ public class LocalMultiplayerGameData : MonoBehaviour
         public int racePosition;
         public string color;
         public Color realColor = new Color();
-<<<<<<< HEAD
-//<<<<<<< HEAD
+
         public int preTurnScore;
-//=======
+
         public int carrotCount=0;
-//>>>>>>> origin/master
-=======
-        public int preTurnScore;
-        public int carrotCount=0;
->>>>>>> origin/master
+
         public int score;
         public bool lastPlayer;
         public List<GameObject> currentSceneObjects = new List<GameObject>();
