2799d7474b89d67c10e7985a52a2fbf5dd961644
William Yang <williamyang93@gmail.com>
2016-03-08 22:51:56 -0800

    Added coin toss script, not complete

---
+++ BUNBUNN/Assets/Scripts/CoinToss.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinToss : MonoBehaviour {
+
+    private Random rand = new Random();
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void chooseBackground()
+    {
+        int number = rand.Next(0, 2);
+        if(number == 0)
+        { 
+            // choose western
+        }
+        else
+        {
+            // choose feudal
+        }
+    }
+}
